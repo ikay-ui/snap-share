d032d64f6c66b3f4f5295e39dbdeb68e01f735d8
ikay-ui <Ikayumar01@gmail.com>
2025-01-09 23:39:06 +0100

    Updated and ready to deploy

--- snap_save/settings.py
+++ snap_save/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snap-share.onrender.com', 'localhost']
 
 ADMIN_EMAIL = 'ikayumar01@gmail.com'
 # Application definition
